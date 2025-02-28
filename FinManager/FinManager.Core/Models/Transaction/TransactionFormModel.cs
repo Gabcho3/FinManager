9c69e1373828f9c61f4fd42298df009f552162ac
Gabriel Dimitrov <gabriel_work@abv.bg>
2025-02-28 22:29:19 +0200

    Add TransactionFormModel

---
+++ FinManager/FinManager.Core/Models/Transaction/TransactionFormModel.cs
@@ -0,0 +1,31 @@
+﻿using FinManager.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+using FinManager.Common.DataConstants;
+
+namespace FinManager.Core.Models.Transactions
+{
+    public class TransactionFormModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public TransactionType TransactionType { get; set; }
+
+        [Required]
+        [Range(AmountConstants.AmountMin, AmountConstants.AmountMax)]
+        public decimal Amount { get; set; }
+
+        [MaxLength(TransactionConstants.DescriptionMaxLength)]
+        public string? Description { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public Guid CatagoryId { get; set; }
+    }
+}
